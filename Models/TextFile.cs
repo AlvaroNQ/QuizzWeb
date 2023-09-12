21646ecc70102a8391f90043b388748aa74990a8
Alvaro Navarro Quijada <alvaro.navarro@tprs.stud.vu.lt>
2023-09-12 11:31:20 +0300

    Successfully reading txt file from a relative path :D

--- Models/TextFile.cs
+++ Models/TextFile.cs
@@ -13,14 +13,14 @@ namespace QuizzWeb.Models
             try
             {
                 Console.WriteLine("Attempt to read file;");
-                content = File.ReadAllText("C:\\Users\\Alvaro\\myStuff\\university\\Software Engineering I-II\\InternationalTeam\\QuizzWeb\\wwwroot\\TextFile.txt");
+                content = File.ReadAllText("./Models/TextFile.txt");
                 Console.WriteLine("File Content:");
                 Console.WriteLine(content);
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred while reading the file: " + e.Message);
-                this.content = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                this.content = "Error: couldnt Read the specified file";
                 //this.content = "Error: Couldnt Open the file";
 
             }
