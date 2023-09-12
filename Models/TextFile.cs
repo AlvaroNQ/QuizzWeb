83f52a888949980e2269ef28887a5367f645fe3c
Alvaro Navarro Quijada <alvaro.navarro@tprs.stud.vu.lt>
2023-09-12 10:42:31 +0300

    Added TextFile Model

---
+++ Models/TextFile.cs
@@ -0,0 +1,22 @@
+﻿namespace QuizzWeb.Models
+{
+    public class TextFile
+    {
+        public string? content = null;
+        public TextFile()
+        {
+            try
+            {
+                Console.WriteLine("Attempt to read file;");
+                content = File.ReadAllText("./TextFile.txt");
+                Console.WriteLine("File Content:");
+                Console.WriteLine(content);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file: " + e.Message);
+                this.content = "Error: Couldnt Open the file";
+            }
+        }
+    }
+}
