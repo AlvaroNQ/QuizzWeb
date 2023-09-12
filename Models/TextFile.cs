a80638e3c7f05041b6ce29d9680805047e8a0bc5
Alvaro Navarro Quijada <alvaro.navarro@tprs.stud.vu.lt>
2023-09-12 11:54:02 +0300

    Working version, Text filled and fitted

--- Models/TextFile.cs
+++ Models/TextFile.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using static System.Net.Mime.MediaTypeNames;
 using System.Web;
+using System.Text;
 
 namespace QuizzWeb.Models
 {
@@ -13,14 +14,14 @@ namespace QuizzWeb.Models
             try
             {
                 Console.WriteLine("Attempt to read file;");
-                content = File.ReadAllText("./Models/TextFile.txt");
+                content = File.ReadAllText("./Models/TextFile.txt", Encoding.GetEncoding("iso-8859-1"));
                 Console.WriteLine("File Content:");
                 Console.WriteLine(content);
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred while reading the file: " + e.Message);
-                this.content = "Error: couldnt Read the specified file";
+                this.content = "Error: couldnt Read the file specified";
                 //this.content = "Error: Couldnt Open the file";
 
             }
