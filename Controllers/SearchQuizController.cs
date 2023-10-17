3aac61f4d1f040b6533cc5175026af51b2e95fa1
Giulio Naspro <giulio.naspro@studenti.unimi.it>
2023-10-17 17:43:00 +0300

    Search Data using LINQ

--- Controllers/SearchQuizController.cs
+++ Controllers/SearchQuizController.cs
@@ -1,6 +1,8 @@
 ﻿// Controllers/QuizController.cs
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Linq;
+
 namespace QuizzWeb.Models;
 public class SearchQuizController : Controller
 {
@@ -12,26 +14,47 @@ public class SearchQuizController : Controller
     [HttpPost]
     public IActionResult LoadQuiz(string quizTitle)
     {
-        var jsonFiles = Directory.EnumerateFiles("Assets", "*.json");
+        var jsonFiles = Directory.EnumerateFiles("./Assets/", "*.json");
 
-        foreach (var jsonFile in jsonFiles)
+        // Deserialize each file content into QuizModel and project into a list.
+        var quizzes = jsonFiles.Select(file =>
         {
-            string jsonContent = System.IO.File.ReadAllText(jsonFile);
-            JObject jsonObj = JObject.Parse(jsonContent);
+            string content = System.IO.File.ReadAllText(file);
+            return JsonSerializer.Deserialize<QuizModel>(content);
+        }).ToList();
 
-            if (jsonObj["Title"].ToString() == quizTitle)
+        // Find the matching quiz using LINQ
+        var matchingQuiz = quizzes.FirstOrDefault(q => q.Title == quizTitle);
+
+        if (matchingQuiz != null)
+        {
+            // If a quiz is found, update its properties
+            matchingQuiz.Metadata = new QuizMetadataModel
             {
-                //Quiz is opened
-                jsonObj["Metadata"]["Status"] = QuizStatusModel.InProgress.ToString();
-                jsonObj["Metadata"]["lastOpened"] = DateTime.Now;
+                creationDate = matchingQuiz.Metadata.Value.creationDate,
+                Status = QuizStatusModel.InProgress,
+                lastOpened = DateTime.Now,
+            };
 
-                System.IO.File.WriteAllText(jsonFile, jsonObj.ToString());
-                QuizModel quizModel = jsonObj.ToObject<QuizModel>();
-                
-                return View("LoadQuiz", quizModel);
-            }
+            // Find the file path of the matching quiz
+            string quizFilePath = jsonFiles.First(file =>
+            {
+                string content = System.IO.File.ReadAllText(file);
+                var quiz = JsonSerializer.Deserialize<QuizModel>(content);
+                return quiz.Title == quizTitle;
+            });
+
+            // Serialize the updated quiz back to JSON and save it to the file
+            string updatedQuizJson = JsonSerializer.Serialize(matchingQuiz);
+            System.IO.File.WriteAllText(quizFilePath, updatedQuizJson);
+
+            // Pass the updated quiz into the view
+            return View("LoadQuiz", matchingQuiz);
         }
+
         return Content("Quiz not found");
     }
-
 }
+
+
+    
